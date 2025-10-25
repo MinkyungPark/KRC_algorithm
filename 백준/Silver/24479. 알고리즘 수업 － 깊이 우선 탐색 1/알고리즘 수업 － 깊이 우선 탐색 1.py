457a46cf4693a0b6c7f2a89397f13dee723ee9cd
박민경 <parkminkyung32@gmail.com>
2025-10-26 01:00:21 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 412 ms, Memory: 138892 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,28 @@
+N, M, R = map(int, input().split())
+g = {i: [] for i in range(1, N + 1)}
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    g[u].append(v)
+    g[v].append(u)
+
+for k, v in g.items():
+    v.sort(reverse=True)
+
+visited = [0] * (N + 1) 
+cnt = 1
+
+stack = [R]
+while stack:
+    u = stack.pop()
+    if visited[u]:
+        continue
+    visited[u] = cnt
+    cnt += 1
+
+    for v in g[u]:
+        if not visited[v]:
+            stack.append(v)
+
+
+print('\n'.join(map(str, visited[1:])))
